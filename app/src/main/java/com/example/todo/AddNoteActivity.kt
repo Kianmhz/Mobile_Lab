c6c1c73e1db7c5d841afcffafac03cc1a5c3e312
Abdul <Abdulkarim.noorzaie@gmail.com>
2025-09-21 21:05:30 -0400

    Added tasktitle, date, and description buttons and formatted them. Also added variables

--- app/src/main/java/com/example/todo/AddNoteActivity.kt
+++ app/src/main/java/com/example/todo/AddNoteActivity.kt
@@ -1,6 +1,8 @@
 package com.example.todo
 
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
 
@@ -14,5 +16,11 @@ class AddNoteActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+
+        val taskTitle = findViewById<EditText>(R.id.tasktitle)
+        val taskDate = findViewById<DatePicker>(R.id.taskDate)
+        val taskDesc = findViewById<EditText>(R.id.taskDesc)
+
+
     }
 }
